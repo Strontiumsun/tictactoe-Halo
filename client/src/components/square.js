ee7dd5a07c76adde9e3873a46f81d563eaf014fa
Strontiumsun <izzyhall25@gmail.com>
2019-07-02 13:05:48 -0500

    used a tutorial to make this, plus added a new game feature

---
+++ client/src/components/square.js
@@ -0,0 +1,111 @@
+import React, { Component } from "react";
+
+
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
+}
+
+
+class Board extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: [null, null, null, null, null, null, null, null, null],
+            xStarts: true
+        }
+    }
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xStarts ? "X" : "O"
+        this.setState({
+            squares: squares,
+            xStarts: !this.state.xStarts
+        })
+
+    }
+
+    renderSquare(i) {
+        return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />
+    }
+    render() {
+        const winner = winGame(this.state.squares);
+        let status;
+        if (winner) {
+            status = "Winner: " + winner;
+            timerNew();
+        } else {
+            status = 'Next player: ' + (this.state.xStarts ? "X" : "O");
+        }
+
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+function winGame(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+            return (squares[a])
+    }
+
+    return null;
+}
+
+
+function timerNew() {
+    setTimeout(newGame, 1000)
+}
+function newGame() {
+    window.location.reload()
+}
+
+
+class Game extends Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Game;
+
